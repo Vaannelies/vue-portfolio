a67976ce8ebdb3ba48e7c870e49c1396e805a8be
Annelies <annelies2000@outlook.com>
2025-08-05 21:17:16 +0200

    build

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -4,10 +4,10 @@
       <div class="footer__contact-container">
           <h2 class="text-l">let's connect</h2>
           <div class="footer__icons">
-              <a target="_blank" href="https://linkedin.com/in/anneliesvaandrager" class="footer__icon"><img src="/img/linkedin-app-white-icon.png"/></a>
-              <a target="_blank" href="https://github.com/Vaannelies" class="footer__icon"><img src="/img/github-white-icon.png"/></a>
-              <a target="_blank" href="https://instagram.com/groetjesvanannelies" class="footer__icon"><img src="/img/instagram-white-icon.png"/></a>
-              <a target="_blank" href="mailto:annelies.vaandrager@outlook.com" class="footer__icon"><img src="/img/email-white-icon.png"/></a>
+              <a target="_blank" href="https://linkedin.com/in/anneliesvaandrager" class="footer__icon"><img src="../assets/img/linkedin-app-white-icon.png"/></a>
+              <a target="_blank" href="https://github.com/Vaannelies" class="footer__icon"><img src="../assets/img/github-white-icon.png"/></a>
+              <a target="_blank" href="https://instagram.com/groetjesvanannelies" class="footer__icon"><img src="../assets/img/instagram-white-icon.png"/></a>
+              <a target="_blank" href="mailto:annelies.vaandrager@outlook.com" class="footer__icon"><img src="../assets/img/email-white-icon.png"/></a>
           </div>
       </div>
     </div>
